64a3ee70159137ccdd890bdf918f020b5fc74456
Tetiana Chykalova <chikalova.t@gmail.com>
2022-12-16 10:28:36 +0100

    refactor filter

--- src/components/Users/Filter/Filter.js
+++ src/components/Users/Filter/Filter.js
@@ -3,6 +3,7 @@ import React from 'react';
 function Filter(props) {
 
     const onFilterValueChange = (e) => {
+        // console.log(e.target.value)
         props.filterValurSelect(e.target.value)
     }
 
@@ -11,7 +12,8 @@ function Filter(props) {
             <select name="gender" id="" onChange={onFilterValueChange}>
                 <option value="All">All</option>
                 <option value="Male">Male</option>
-                <option value="Female">Female</option>            </select>
+                <option value="Female">Female</option>
+            </select>
         </div>
     );
 }
--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,11 +1,10 @@
 import React, {useState} from 'react';
 import UserItem from "./UserItem";
 import Filter from "./Filter/Filter";
-import Pagination from "./Pagination/Pagination";
 
 function UsersList(props) {
     let item = props.count
-    // console.log(item.length)
+    console.log(item)
 
     const[filterText, setFilterText] = useState('all')
 
@@ -14,38 +13,54 @@ function UsersList(props) {
 
     const lastPostIndex = currentPage * userPage
     const firstPostIndex = lastPostIndex - userPage
-    const currentPost = item.slice(firstPostIndex, lastPostIndex)
+    const currentUser = item.slice(firstPostIndex, lastPostIndex)
 
     let pages = []
-    for (let i = 1; i <= Math.ceil(item.length/userPage); i++) {
-        pages.push(i)
-    }
 
-    let pageButton = pages.map((value, index) => {
-        return <button key={index} onClick={() => {setCurrentPage(value)}} >{value}</button>
-    })
-
-    let filterUsers = currentPost.filter((user) => {
-        if(filterText === 'Male') {
-            return user.isAvaible = false
-        } else if(filterText === 'Female') {
-            return user.isAvaible = true
+    // filter
+    let filterUsers = item.filter((user) => {
+        let gender = user.gender
+        if(gender === 'Male') {
+            return user.gender === "Mail"
+        } else if (gender === "Female") {
+            return user.gender === "Female"
         } else {
             return user
         }
     })
+    // console.log(filterUsers)
     let onFilterValueSelect = (filterVal) => {
         setFilterText(filterVal)
+        console.log(filterVal)
+        console.log(filterText)
+    }
+    const onFilterValueChange = (e) => {
+        // console.log(e.target.value)
+        onFilterValueSelect(e.target.value)
     }
 
-    let addNewUserItem = filterUsers.map( (user) =>  <UserItem props={user} key={user.id}/> )
+    // AddUserItem
+    let addNewUserItem = item.map( (user) =>  <UserItem props={user} key={user.id}/> )
 
-    console.log(filterUsers)
-    console.log(userPage)
+    // Pagination
+    for (let i = 1; i <= Math.ceil(item.length/userPage); i++) {
+        pages.push(i)
+    }
+    let pageButton = pages.map((value, index) => {
+        return <button key={index} onClick={() => {setCurrentPage(value)}} >{value}</button>
+    })
 
     return (
         <div>
-            <Filter filterValurSelect={onFilterValueSelect}/>
+            {/*<Filter filterValurSelect={onFilterValueSelect}/>*/}
+            <div>
+                <select name="gender" id="" onChange={onFilterValueChange}>
+                    <option value="All">All</option>
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                </select>
+            </div>
+
             {addNewUserItem}
             {pageButton}
         </div>
