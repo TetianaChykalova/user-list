2263d6c2629b2ac7ffc7309199dffe4c3b438c49
Tetiana Chykalova <chikalova.t@gmail.com>
2022-12-16 10:30:43 +0100

    without filter

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,12 +1,9 @@
 import React, {useState} from 'react';
 import UserItem from "./UserItem";
-import Filter from "./Filter/Filter";
 
 function UsersList(props) {
     let item = props.count
     console.log(item)
-
-    const[filterText, setFilterText] = useState('all')
 
     const[currentPage, setCurrentPage] = useState(1)
     const[userPage, setUserPage] = useState(4)
@@ -17,30 +14,8 @@ function UsersList(props) {
 
     let pages = []
 
-    // filter
-    let filterUsers = item.filter((user) => {
-        let gender = user.gender
-        if(gender === 'Male') {
-            return user.gender === "Mail"
-        } else if (gender === "Female") {
-            return user.gender === "Female"
-        } else {
-            return user
-        }
-    })
-    // console.log(filterUsers)
-    let onFilterValueSelect = (filterVal) => {
-        setFilterText(filterVal)
-        console.log(filterVal)
-        console.log(filterText)
-    }
-    const onFilterValueChange = (e) => {
-        // console.log(e.target.value)
-        onFilterValueSelect(e.target.value)
-    }
-
     // AddUserItem
-    let addNewUserItem = item.map( (user) =>  <UserItem props={user} key={user.id}/> )
+    let addNewUserItem = currentUser.map( (user) =>  <UserItem props={user} key={user.id}/> )
 
     // Pagination
     for (let i = 1; i <= Math.ceil(item.length/userPage); i++) {
@@ -52,15 +27,6 @@ function UsersList(props) {
 
     return (
         <div>
-            {/*<Filter filterValurSelect={onFilterValueSelect}/>*/}
-            <div>
-                <select name="gender" id="" onChange={onFilterValueChange}>
-                    <option value="All">All</option>
-                    <option value="Male">Male</option>
-                    <option value="Female">Female</option>
-                </select>
-            </div>
-
             {addNewUserItem}
             {pageButton}
         </div>
