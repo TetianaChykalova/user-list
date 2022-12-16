fa5e067b61ed6110430ef54c6f0eae8dbc8fa61d
Tetiana Chykalova <chikalova.t@gmail.com>
2022-12-16 17:17:13 +0100

    failed filter

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import UserItem from "./UserItem";
 import s from './UserList.module.css'
 import Categories from "./Filter/Categories";
@@ -6,16 +6,6 @@ import Categories from "./Filter/Categories";
 function UsersList(props) {
     let item = props.count
     // console.log(item)
-
-    // filter
-    const [data, setData] = useState(Categories)
-
-    const filterResult = (item) => {
-        Categories.filter(getItem => {
-            return getItem.gender === item
-        })
-        setData(Categories)
-    }
 
     const[currentPage, setCurrentPage] = useState(1)
     const userPage = 4
@@ -25,6 +15,37 @@ function UsersList(props) {
     const currentUser = item.slice(firstPostIndex, lastPostIndex)
 
     let pages = []
+
+    // filters
+    const gender = [
+        {
+            id: 0,
+            gender: 'All',
+        },
+        {
+            id: 1,
+            gender: 'Male',
+        },
+        {
+            id: 2,
+            gender: 'Female',
+        },
+    ]
+    const [filtered, setFiltered] = useState(gender)
+
+    console.log(filtered)
+
+    let genderFilter = (genderInput) => {
+        if(genderInput === 'All') {
+            setFiltered(gender)
+        } else {
+            let newGender = [...gender].filter(item => item.gender === genderInput)
+            setFiltered(newGender)
+        }
+    }
+    useEffect(() => {
+        setFiltered(currentUser)
+    }, [])
 
     // AddUserItem
     let addNewUserItem = currentUser.map( (user) =>  <UserItem props={user} key={user.id}/> )
@@ -40,15 +61,12 @@ function UsersList(props) {
     return (
         <div className={s.wrapper}>
             <div>
-                <button onClick={() => setData(Categories)}>All</button>
-                <button onClick={() => filterResult('Mail')}>Male</button>
-                <button onClick={() => filterResult('Female')}>Female</button>
+                <button onClick={() => {genderFilter('all')}}>All</button>
+                <button onClick={() => {genderFilter('Male')}}>Male</button>
+                <button onClick={() => {genderFilter('Female')}}>Female</button>
             </div>
             <div className={s.list}>
                 {addNewUserItem}
-                {/*{data.map((value) => {*/}
-                {/*    const {id, gender} = value*/}
-                {/*})}*/}
             </div>
             <div className={s.pages}>
                 {pageButton}
